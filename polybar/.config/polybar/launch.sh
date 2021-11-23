89d5600afdf7a00e72f1947d7024b0e2848f04a8
Nick <nickd2225@gmail.com>
2021-11-22 23:07:42 -0800

    polybar updates

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -2,5 +2,8 @@
 
 killall -q polybar
 
-# polybar -c ${HOME}/.config/polybar/config bar1 &
-polybar top 2>&1 | tee -a /tmp/polybar1.log & disown
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+polybar main -c ${HOME}/.config/polybar/config.ini
+
